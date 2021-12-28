1f96741917fdfcc389cb7d99379c47f1922ae603
Anatoli Rak <Rack.anatoly@gmail.com>
2021-12-28 13:16:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let view = {
+    displayMessage: function(msg){
+        let messageArea = document.getElementById('messageArea');
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (loaction){
+
+    },
+    displayMiss: function (location){
+
+    }
+
+};
